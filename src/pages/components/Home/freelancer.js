97b56d1903827c818380163d6dd1b64a8ad098f0
bydefaultiloveyou <mikomeysa05@gmail.com>
2022-12-14 15:24:48 +0700

    solve bug wa

--- src/pages/components/Home/freelancer.js
+++ src/pages/components/Home/freelancer.js
@@ -9,7 +9,7 @@ const Freelance = () => {
         Saya juga membuka jasa pembuatan website jika temen temen tertarik bisa
         chat saya di
       </p>
-      <Link href={"wa.me/6287895013427"}>
+      <Link href={"https://wa.me/6287895013427"}>
         <p className="flex item-center mt-3 rounded-3xl justify-center py-2 px-3 w-40 bg-green-500 font-bold text-white">
           <BsWhatsapp className="mr-3 text-xl" /> Whatsapp
         </p>
